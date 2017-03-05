c03e8f69c7d377ca54036c9649626d79b6778ca7
Sumnut <mattoneill66@yahoo.ca>
2017-03-04 21:16:54 -0500

    BTScan Bucket system created

--- platforms/android/assets/www/js/BTScan.js
+++ platforms/android/assets/www/js/BTScan.js
@@ -2,6 +2,7 @@ var BT_Devices = {};
 var BT_ACCEPTED_LIST = [];
 var BT_INIT = false;
 var BT_ACCEPTED_DEVICE_REFRESH_RATE = 100;
+var BT_PREVIOUS_VALUE = {};
 
 function AddDevice(device)
 {
@@ -9,9 +10,8 @@ function AddDevice(device)
         BT_Devices[device.name] = {}
         BT_Devices[device.name].rssi = device.rssi;
         BT_Devices[device.name].id = device.id;
-        console.log(device);
+        BT_PREVIOUS_VALUE[device.name] = rssi;
     }
-    console.log("AddDevice()");
 }
 
 function callbackAcceptedDeviced()
@@ -20,18 +20,37 @@ function callbackAcceptedDeviced()
         var name = item.name;
         if (BT_Devices[name])
         {
-            var size = -1*BT_Devices[name].rssi;
-            if(size > 120)
-                size = 120;
-            if(size < 20)
-                size = 20;
-            size = (100 - (size - 20))/ 100;
+            var bucketSize = 4;
+            var rssi = BT_Devices[name].rssi;
+            var size = rssi - BT_PREVIOUS_VALUE[name];
+            if (rssi > -40 &&  BT_PREVIOUS_VALUE[name] > -50)
+                if (size > 35)
+                    size = BT_PREVIOUS_VALUE[name];
+                else
+                {
+                    size = BT_Devices[name].rssi;
+                    BT_PREVIOUS_VALUE[name] = BT_Devices[name].rssi;
+                }
+            else
+            {
+                if (size > 15)
+                    size = BT_PREVIOUS_VALUE[name];
+                else
+                {
+                    size = BT_Devices[name].rssi;
+                    BT_PREVIOUS_VALUE[name] = BT_Devices[name].rssi;
+                }
+            }
+            if(size  > -90)
+                bucketSize = 3;
+            if(size > -60)
+                bucketSize = 2;
+            if(size > -30)
+                bucketSize = 1;
 
-            item.callback(name, size, BT_Devices[name].id,  -1*BT_Devices[name].rssi);
-            console.log(name, size);
+            item.callback(name, bucketSize, BT_Devices[name].id,  BT_Devices[name].rssi);
         }
     });
-    console.log("callbackAcceptedDeviced()");
 }
 
 function BTScan_Init(bt_accepted_list, refresh_rate)
